70e3ca1529796e9c82ce4d1cb09402ed01f4b9e2
BrianBarmao <brian@whitelabeltracking.com>
2021-09-15 16:10:58 +0300

    added binary tree example

---
+++ BinaryTreeExamples/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeExamples
+{
+    class Node
+    {
+        public Node LeftNode { get; set; }
+        public Node RightNode { get; set; }
+        public int Data { get; set; }
+    }
+}
---
+++ BinaryTreeExamples/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BinaryTreeExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BinaryTree Implementation\n");
+
+            BinaryTree binaryTree = new BinaryTree();
+            binaryTree.Add(1);
+            binaryTree.Add(2);
+            binaryTree.Add(7);
+            binaryTree.Add(4);
+            binaryTree.Add(10);
+            binaryTree.Add(5);
+            binaryTree.Add(8);
+            binaryTree.Add(6);
+            binaryTree.Add(3);
+
+            Node node = binaryTree.Find(5);
+            int depth = binaryTree.GetTreeDepth();
+
+            Console.WriteLine("PreOrder Transversal");
+            binaryTree.TraversePreOrder(binaryTree.Root);
+            Console.WriteLine();
+
+            Console.WriteLine("InOrder Transversal");
+            binaryTree.TraversePreOrder(binaryTree.Root);
+            Console.WriteLine();
+
+            Console.WriteLine("PostOrder Transversal");
+            binaryTree.TraversePreOrder(binaryTree.Root);
+            Console.WriteLine();
+
+            binaryTree.Remove(7);
+            binaryTree.Remove(8);
+
+
+            Console.WriteLine("PreOrder Traversal After Removing Operation:");
+            binaryTree.TraversePreOrder(binaryTree.Root);
+            Console.WriteLine();
+
+            Console.ReadLine();
+
+        }
+    }
+}
